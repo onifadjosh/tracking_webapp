a1e74ff02deecfe509be13fc7ce14d94fa9be109
onifadjosh <onifadjosh@gmail.com>
2022-08-04 17:42:25 +0100

    accordion repair

--- sripts.js
+++ sripts.js
@@ -11,3 +11,37 @@ document.querySelectorAll("nav-link").forEach(n => n.
     hamburger.classList.remove("active");
     navContent.classList.remove("active");
   }))
+
+
+
+  var acc = document.getElementsByClassName("accordion");
+                    var i;
+                    
+                    for (i = 0; i < acc.length; i++) {
+                      acc[i].addEventListener("click", function() {
+                        this.classList.toggle("active");
+                        var panel = this.nextElementSibling;
+                        if (panel.style.display === "block") {
+                          panel.style.display = "none";
+                        } else {
+                          panel.style.display = "block";
+                        }
+                      });
+                    }
+
+
+
+                    var acc = document.getElementsByClassName("accord");
+                    var i;
+    
+                     for (i = 0; i < acc.length; i++) {
+                        acc[i].addEventListener("click", function() {
+                        this.classList.toggle("active");
+                        var panel = this.nextElementSibling;
+                        if (panel.style.display === "block") {
+                            panel.style.display = "none";
+                        } else {
+                          panel.style.display = "block";
+                        }
+                        });
+                    }
